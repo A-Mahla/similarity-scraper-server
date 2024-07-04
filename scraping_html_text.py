a8e95e204913496dfe321f9f58a15f7bfb9b02d6
A-Mahla <>
2024-07-04 13:26:27 +0200

    feat: optimize ScraperTree score compute to O(n)

--- scraping_html_text.py
+++ scraping_html_text.py
@@ -1,8 +1,8 @@
-from bs4 import BeautifulSoup, Tag, NavigableString
+from bs4 import BeautifulSoup, Tag, NavigableString, PageElement
 from anytree import NodeMixin
-from typing import Optional, Union
+from typing import Optional, Union, Tuple
 import requests
-from pydantic import HttpUrl, BaseModel
+from pydantic import HttpUrl, BaseModel, Field
 from langdetect import detect
 from PIL import Image
 from io import BytesIO
@@ -12,12 +12,20 @@ from urllib.parse import urlparse, urlunparse
 
 
 class Website(BaseModel):
-    url: HttpUrl
-    base_url: str
+    url: HttpUrl = Field(
+        ...,
+        description="The URL of the website to scrape.",
+    )
+
+    @property
+    def base_url(self) -> str:
+        """Compute and return the base URL from the full URL."""
+        parsed_url = urlparse(str(self.url))
+        return urlunparse((parsed_url.scheme, parsed_url.netloc, "", "", "", ""))
 
 
 class ImageProcessor:
-    def __init__(self, base_url):
+    def __init__(self, base_url: str):
         self.base_url = base_url
 
     def correct_relative_url(self, base_url: str, url: str) -> str:
@@ -38,9 +46,9 @@ class ImageProcessor:
 
     def get_raster_image_size(self, url: str):
         try:
-            response = requests.get(url)
+            response = requests.get(url, allow_redirects=True)
             with Image.open(BytesIO(response.content)) as img:
-                return img.size  # (width, height)
+                return img.size
         except Exception as e:
             print(f"Could not retrieve raster image size for {url}: {e}")
             return (0, 0)
@@ -99,35 +107,30 @@ class ScraperGraph:
         "th",
     ]
 
-    @staticmethod
-    def get_base_url(url: HttpUrl) -> str:
-        """Extract the base URL from a full URL."""
-        parsed_url = urlparse(str(url))
-        base_url = urlunparse((parsed_url.scheme, parsed_url.netloc, "", "", "", ""))
-        return base_url
-
     def __init__(self, url: HttpUrl, is_image: bool = False):
-        self.website = Website(url=url, base_url=ScraperGraph.get_base_url(url))
+        self.website = Website(url=url)
+        self.img_proc = ImageProcessor(self.website.base_url)
         response = requests.get(str(self.website.url))
         if response.status_code != 200:
             return None
         self.soup = BeautifulSoup(response.content, "html.parser")
         self.is_image = is_image
-        self.root = self.build_tree(self.soup)
+        self.root, self.best_node = self.build_tree(self.soup)
 
     def build_tree(
         self,
         element: Union[Tag, NavigableString],
         parent: Optional[Node] = None,
         depth: int = 0,
-    ) -> Node:
+        best_node: Optional[Node] = None,
+    ) -> Tuple[Node, Node]:
         node_name = (
             element.name if isinstance(element, Tag) and element.name else "text"
         )
         node = Node(
             node_name,
             data=element,
-            breadth=len([element.children]) if isinstance(element, Tag) else 1,
+            breadth=len(list(element.children)) if isinstance(element, Tag) else 1,
             parent=parent,
         )
         node.score = (
@@ -135,15 +138,22 @@ class ScraperGraph:
             if self.is_image
             else self.text_score(node, node_name)
         )
-        # node.score = node.depth * node.breadth * node.text_density * node.tag_weight
+
+        if best_node is None or node.score > best_node.score:
+            best_node = node
 
         if isinstance(element, Tag):
             for child in element.children:
                 if isinstance(child, Tag) or (
                     isinstance(child, NavigableString) and child.strip()
                 ):
-                    self.build_tree(child, parent=node, depth=depth + 1)
-        return node
+                    _, best_child = self.build_tree(
+                        child, parent=node, depth=depth + 1, best_node=best_node
+                    )
+                    if best_child.score > best_node.score:
+                        best_node = best_child
+
+        return node, best_node
 
     def is_english(self, text: str) -> bool:
         try:
@@ -152,64 +162,36 @@ class ScraperGraph:
             return False
 
     def calculate_text_density(self, node: Node) -> int:
-        if not node.children:
-            text = node.data.get_text(strip=True) if node.data else ""
-            return len(text) if self.is_english(text) else 0
-        return sum(self.calculate_text_density(child) for child in node.children)
+        text = node.data.get_text(strip=True) if node.data else ""
+        return len(text) if self.is_english(text) else 0
 
     def text_score(self, node: Node, node_name: str) -> int:
+        if node_name not in ScraperGraph.relevant_text_tags:
+            return 0
         node.text_density = self.calculate_text_density(node)
-        node.tag_weight = 1 if node_name in ScraperGraph.relevant_text_tags else 0
-        return node.depth * node.breadth * node.text_density * node.tag_weight
-
-    def correct_relative_url(self, base_url: str, relative_url: str) -> str:
-        """Correct relative URLs using the base URL."""
-        if relative_url.startswith("//"):
-            return f"{base_url.split(':')[0]}:{relative_url}"
-        elif relative_url.startswith("/"):
-            return f"{base_url}{relative_url}"
-        elif not urlparse(relative_url).netloc:
-            return f"{base_url}/{relative_url}"
-        else:
-            return relative_url
-
-    def get_image_size(self, url: str):
-        try:
-            url = self.correct_relative_url(self.website.base_url, url)
-            response = requests.get(url)
-            img = Image.open(BytesIO(response.content))
-            return img.size
-        except Exception as e:
-            print(f"Could not retrieve image size for {url}: {e}")
-            return (0, 0)
+        return node.depth * node.breadth * node.text_density
 
     def image_score(self, node: Node) -> float:
         if isinstance(node.data, Tag) and node.name == "img":
             alt_text = node.data.get("alt", "")
             alt_text_density = len(alt_text) if isinstance(alt_text, str) else 1
             src = node.data.get("src", "")
-            img_proc = ImageProcessor(self.website.base_url)
             width, height = (
-                img_proc.get_image_size(src) if isinstance(src, str) else (0, 0)
+                self.img_proc.get_image_size(src) if isinstance(src, str) else (0, 0)
             )
             size_score = width * height
             return (alt_text_density + size_score) / (node.depth + 1)
         else:
             return 0
 
-    def find_best_node(self, node: Node) -> Node:
-        best_node = node
-        for child in node.children:
-            candidate = self.find_best_node(child)
-            if candidate.score > best_node.score:
-                best_node = candidate
-        return best_node
+    def get_best_node(self) -> Node:
+        return self.best_node
 
 
 if __name__ == "__main__":
-    image = False
-    tree = ScraperGraph("https://www.hcompany.ai/", is_image=image)
-    best_node = tree.find_best_node(tree.root)
+    image = True
+    tree = ScraperGraph("https://en.wikipedia.org/wiki/Dick_Schoof", is_image=image)
+    best_node = tree.get_best_node()
     print(f"Best node tag: {best_node.name}")
     if not image:
         print(f"Best node content: {best_node.data.get_text(strip=True)}")
